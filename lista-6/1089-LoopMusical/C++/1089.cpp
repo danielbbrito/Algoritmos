caefaf739d8b4be8498399f1556876aac1602053
Daniel de Brito <danieldebrito@Daniels-MacBook-Air.local>
2022-12-20 18:01:10 -0300

    commit

--- lista-6/1089-LoopMusical/C++/1089.cpp
+++ lista-6/1089-LoopMusical/C++/1089.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
+
+int picos(vector<int> &amostras);
 
 int main()
 {
     // Declarar variáveis
-    int n, h, picos=0;
+    int n, h, soma;
     vector<int> amostras;
 
     // Ler n
@@ -18,33 +19,45 @@ int main()
         for (int i = 0; i < n; i++)
         {
             cin >> h;
-
             amostras.push_back(h);
         }
 
-        // Verificar primeira amostra, verificar restante
-        if ((amostras.front() < amostras.back() && amostras.front() < amostras[1]) || (amostras.front() > amostras.back() && amostras.front() > amostras[1]))
-        {
-            picos++;
-        }
+        int s = 0;
+        // Analisar primeiro ponto
+        if ((amostras.front() > amostras.back() && amostras.front() > amostras[1]) 
+        || (amostras.front() < amostras.back() && amostras.front() < amostras[1])) s++;
 
+        // Analisar ultimo ponto
+        if ((amostras.back() > amostras.front() && amostras.back() > *(amostras.end() - 1)) 
+        || (amostras.back() < amostras.front() && amostras.back() < *(amostras.end() - 1))) s++;
 
+        // Iterar sobre o vetor
+        for (int i = 0; i < amostras.size(); i++)
+            if ((amostras[i] > amostras[i + 1] && amostras[i] > amostras[i - 1]) || (amostras[i] > amostras[i + 1] && amostras[i] > amostras[i - 1]))
+                s++;
 
-        for (auto itr = amostras.begin() + 1; itr != amostras.end(); itr++)
-        {
-            if (*(itr - 1) < *itr && *(itr + 1) < *itr || *(itr - 1) > *itr && *(itr + 1) > *itr)
-            {
-                picos++;
-            }
-        }
-
-        cout << picos << endl;
-
-        picos = 0;
-        amostras.clear();
-
+        cout << s << endl;
         cin >> n;
     }
 
-        return 0;
+    return 0;
+}
+
+int picos(vector<int> &amostras)
+{
+    int s = 0;
+    // Analisar primeiro ponto
+    if ((amostras.front() > amostras.back() && amostras.front() > amostras[1]) 
+    || (amostras.front() < amostras.back() && amostras.front() < amostras[1])) s++;
+
+    // Analisar ultimo ponto
+    if ((amostras.back() > amostras.front() && amostras.back() > *(amostras.end() - 1)) 
+    || (amostras.back() < amostras.front() && amostras.back() < *(amostras.end() - 1))) s++;
+
+    // Iterar sobre o vetor
+    for (int i = 0; i < amostras.size() - 1; i++)
+        if ((amostras[i] > amostras[i + 1] && amostras[i] > amostras[i - 1]) || (amostras[i] > amostras[i + 1] && amostras[i] > amostras[i - 1]))
+            s++;
+        
+    return s;
 }
